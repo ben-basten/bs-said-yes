5f1a7f6299e084b45d9e5bc7b198bea1a1e0df8f
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-17 18:30:04 -0500

    wip: proof of concept

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,21 @@
 <template>
   <ContentContainer>
-    <Heading as="h1">Mollit veniam laboris sit veniam commodo</Heading>
-    <Heading as="h2">Mollit veniam laboris sit veniam commodo</Heading>
-    <Heading as="h3">Mollit veniam laboris sit veniam commodo</Heading>
-    <Heading as="h4">Mollit veniam laboris sit veniam commodo</Heading>
-    <Heading as="h5">Mollit veniam laboris sit veniam commodo</Heading>
-    <Heading as="h6">Mollit veniam laboris sit veniam commodo</Heading>
-    <div>
-      <a href="https://www.kagi.com">Sample link</a>
-    </div>
+    <Heading as="h1">data: {{ data?.fields.cmsName }}</Heading>
   </ContentContainer>
 </template>
+
+<script setup lang="ts">
+import * as contentful from "contentful";
+const { cmsSpace, cmsApiKey, cmsEnv } = useRuntimeConfig();
+const { data } = useAsyncData("landing", async () => {
+  const client = contentful.createClient({
+    space: cmsSpace,
+    accessToken: cmsApiKey,
+    environment: cmsEnv,
+  });
+  return client.getEntry<{
+    fields: { cmsName: string };
+    contentTypeId: string;
+  }>("1oC9jdPQvkqvDi6Qd2phIl");
+});
+</script>
