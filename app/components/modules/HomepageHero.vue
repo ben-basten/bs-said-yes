7a8af1441dc6338ec65859766e6fa7a429954b19
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-20 07:11:44 -0500

    chore: setup content containers

--- app/components/modules/HomepageHero.vue
+++ app/components/modules/HomepageHero.vue
@@ -1,15 +1,18 @@
 <template>
-  <section>
+  <ContentContainer as="section">
     <p>{{ data.heading }}</p>
     <RichTextRenderer :json="data.copy?.json" />
-    <ContentfulImage v-if="data.image?.url" :src="data.image?.url" :alt="data.image?.description ?? undefined" :sizes="undefined" />
-  </section>
+    <ContentfulImage
+      v-if="data.image?.url"
+      :src="data.image?.url"
+      :alt="data.image?.description ?? undefined"
+      :sizes="undefined"
+    />
+  </ContentContainer>
 </template>
 
 <script setup lang="ts">
-import type {
-  ModuleProps,
-} from "~/types/module";
+import type { ModuleProps } from "~/types/module";
 import type { HomepageHeroFragment } from "~~/shared/types/graphql";
 
 const { data } = defineProps<ModuleProps<HomepageHeroFragment>>();
