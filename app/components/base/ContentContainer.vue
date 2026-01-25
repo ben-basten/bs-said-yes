76d5a60d14002c3d9a452b940979881765a16bd2
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-25 01:17:03 -0500

    wip: grid layout

--- app/components/base/ContentContainer.vue
+++ app/components/base/ContentContainer.vue
@@ -3,13 +3,17 @@
     :is="as"
     data-name="container"
     class="px-side-mobile md:px-side mx-auto w-full max-w-max-width"
+    :class="{
+      'grid grid-cols-12 gap-x-0 md:gap-x-5': grid,
+    }"
   >
     <slot />
   </component>
 </template>
 
 <script setup lang="ts">
-const { as = "div" } = defineProps<{
+const { as = "div", grid = false } = defineProps<{
   as?: "div" | "section";
+  grid?: boolean;
 }>();
 </script>
