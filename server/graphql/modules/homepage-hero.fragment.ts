f429c5541c4173398caf605fe40ee0bce3517d73
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-25 01:47:35 -0500

    wip: homepage hero fragment

--- server/graphql/modules/homepage-hero.fragment.ts
+++ server/graphql/modules/homepage-hero.fragment.ts
@@ -2,12 +2,18 @@ import { gql } from "graphql-request";
 
 export const HOMEPAGE_HERO_FRAGMENT = gql`
   fragment HomepageHero on HomepageHero {
+    eyebrow
     heading
     copy {
       json
     }
     image {
       ...Image
+    }
+    imagesCollection(limit: 5) {
+      items {
+        ...Image
+      }
     }
   }
 `;
