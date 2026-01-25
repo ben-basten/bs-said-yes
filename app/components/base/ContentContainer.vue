9208a455b990b671971a9cc302c4881f7ed8bfe3
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-25 02:41:54 -0500

    wip: utility class

--- app/components/base/ContentContainer.vue
+++ app/components/base/ContentContainer.vue
@@ -4,7 +4,7 @@
     data-name="container"
     class="px-side-mobile md:px-side mx-auto w-full max-w-max-width"
     :class="{
-      'grid grid-cols-12 gap-x-0 md:gap-x-5': grid,
+      'grid-12': grid,
     }"
   >
     <slot />
