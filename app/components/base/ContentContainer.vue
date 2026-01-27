d6646fea6f6644be9170ce0613d1079f6c387ce0
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-27 16:27:22 -0500

    wip: remove content container wide mode

--- app/components/base/ContentContainer.vue
+++ app/components/base/ContentContainer.vue
@@ -2,11 +2,9 @@
   <component
     :is="as"
     data-name="container"
-    class="mx-auto w-full max-w-max-width"
+    class="mx-auto w-full max-w-max-width px-side-mobile md:px-side"
     :class="{
       'grid-12': grid,
-      'px-side-mobile md:px-side': padding === 'default',
-      'px-0 md:px-side-sm': padding === 'sm',
     }"
   >
     <slot />
@@ -14,13 +12,8 @@
 </template>
 
 <script setup lang="ts">
-const {
-  as = "div",
-  grid = false,
-  padding = "default",
-} = defineProps<{
+const { as = "div", grid = false } = defineProps<{
   as?: "div" | "section";
   grid?: boolean;
-  padding?: "sm" | "default";
 }>();
 </script>
