303570b238490980a42a2ed47d80d95c7e28f1af
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-27 17:14:46 -0500

    wip: refactor 50/50

--- app/components/modules/FiftyFifty.vue
+++ app/components/modules/FiftyFifty.vue
@@ -1,60 +1,6 @@
 <template>
-  <ContentContainer
-    as="section"
-    :padding="background === 'Tan' ? 'sm' : 'default'"
-    class="mb-vertical md:mb-vertical-lg"
-  >
-    <div
-      class="grid-12 gap-y-7"
-      :class="{
-        'bg-secondary px-side-offset md:rounded-2xl overflow-hidden py-vertical':
-          background === 'Tan',
-      }"
-    >
-      <div
-        class="col-span-12 max-md:order-2 md:col-span-5 max-w-copy flex flex-col justify-center gap-y-6 mb-8 md:mb-0"
-        :class="{
-          'md:order-2 md:col-start-8': align === 'Left',
-        }"
-      >
-        <Heading as="h2">{{ data.heading }}</Heading>
-        <p class="type-eyebrow order-first">{{ data.eyebrow }}</p>
-        <RichTextRenderer :json="data?.copy?.json" />
-        <div class="flex flex-wrap gap-x-5 gap-y-3">
-          <InlineLink
-            v-if="data.callToAction?.url"
-            :href="data.callToAction.url"
-            :external="data.callToAction.external"
-            class="button button-md"
-          >
-            {{ data.callToAction.text }}
-          </InlineLink>
-          <InlineLink
-            v-if="data.secondaryCallToAction?.url"
-            :href="data.secondaryCallToAction.url"
-            :external="data.secondaryCallToAction.external"
-            class="button-secondary button-md"
-          >
-            {{ data.secondaryCallToAction.text }}
-          </InlineLink>
-        </div>
-      </div>
-      <div
-        v-if="data.image?.url"
-        class="col-span-12 max-md:order-1 md:col-span-6 my-auto relative"
-        :class="{
-          'md:col-start-7': align === 'Right',
-        }"
-      >
-        <ContentfulImage
-          :src="data.image.url"
-          :alt="data.image.description ?? ''"
-          sizes="95vw md:50vw lg:730px"
-          class="object-cover aspect-4/3 rounded-xl w-full"
-        />
-      </div>
-    </div>
-  </ContentContainer>
+  <FiftyFiftyTan v-if="background === 'Tan'" :data="data" />
+  <FiftyFiftyWhite v-else :data="data" />
 </template>
 
 <script setup lang="ts">
@@ -63,6 +9,5 @@ import type { FiftyFiftyFragment } from "~~/shared/types/graphql";
 
 const { data } = defineProps<ModuleProps<FiftyFiftyFragment>>();
 
-const align = computed(() => data.imageAlignment as "Left" | "Right");
 const background = computed(() => data.background as "White" | "Tan");
 </script>
---
+++ app/components/modules/FiftyFiftyWhite.vue
@@ -0,0 +1,57 @@
+<template>
+  <ContentContainer as="section" class="mb-vertical md:mb-vertical-lg">
+    <div class="grid-12 gap-y-7">
+      <div
+        class="col-span-12 max-md:order-2 md:col-span-5 max-w-copy flex flex-col justify-center gap-y-6 mb-8 md:mb-0"
+        :class="{
+          'md:order-2 md:col-start-8': align === 'Left',
+        }"
+      >
+        <Heading as="h2">{{ data.heading }}</Heading>
+        <p class="type-eyebrow order-first">{{ data.eyebrow }}</p>
+        <RichTextRenderer :json="data?.copy?.json" />
+        <div class="flex flex-wrap gap-x-5 gap-y-3">
+          <InlineLink
+            v-if="data.callToAction?.url"
+            :href="data.callToAction.url"
+            :external="data.callToAction.external"
+            class="button button-md"
+          >
+            {{ data.callToAction.text }}
+          </InlineLink>
+          <InlineLink
+            v-if="data.secondaryCallToAction?.url"
+            :href="data.secondaryCallToAction.url"
+            :external="data.secondaryCallToAction.external"
+            class="button-secondary button-md"
+          >
+            {{ data.secondaryCallToAction.text }}
+          </InlineLink>
+        </div>
+      </div>
+      <div
+        v-if="data.image?.url"
+        class="col-span-12 max-md:order-1 md:col-span-6 my-auto relative flex items-center justify-center overflow-clip"
+        :class="{
+          'md:col-start-7': align === 'Right',
+        }"
+      >
+        <ContentfulImage
+          :src="data.image.url"
+          :alt="data.image.description ?? ''"
+          sizes="95vw md:50vw lg:730px"
+          class="object-cover aspect-4/3 w-full md:h-full rounded-xl"
+        />
+      </div>
+    </div>
+  </ContentContainer>
+</template>
+
+<script setup lang="ts">
+import type { ModuleProps } from "~/types/module";
+import type { FiftyFiftyFragment } from "~~/shared/types/graphql";
+
+const { data } = defineProps<ModuleProps<FiftyFiftyFragment>>();
+
+const align = computed(() => data.imageAlignment as "Left" | "Right");
+</script>
