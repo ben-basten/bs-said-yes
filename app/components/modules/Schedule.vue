26469b035e546d131ac4a562c5baaec3f79f5316
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-28 12:17:50 -0500

    fix: smaller schedule grid columns

--- app/components/modules/Schedule.vue
+++ app/components/modules/Schedule.vue
@@ -14,7 +14,7 @@
     </div>
 
     <div
-      class="col-span-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
+      class="col-span-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
     >
       <div
         v-for="(event, index) in events"
