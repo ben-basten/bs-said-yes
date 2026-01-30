d987f1f592e7e418cb920bbae7f0ee89cfd97c68
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-29 22:25:27 -0500

    chore: add an error page

---
+++ app/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLayout>
+    <ContentContainer class="mt-10">
+      <div
+        class="max-w-160 text-center flex flex-col gap-7 md:gap-10 items-center mx-auto my-auto"
+      >
+        <Heading as="h1" variant="h2">{{ message }}</Heading>
+        <InlineLink href="/" class="button button-lg" @click="handleError">
+          Go back home
+        </InlineLink>
+      </div>
+    </ContentContainer>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const { error } = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const message = computed(() => {
+  if (error?.statusCode === 404) {
+    return "Ope! That page could not be found.";
+  } else {
+    return "Something unexpected went wrong.";
+  }
+});
+const handleError = () => clearError({ redirect: "/" });
+</script>
