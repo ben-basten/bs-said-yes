adcf1e70a8750f92b0c1347768cd195c2f4957ef
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-30 07:39:11 -0500

    wip: remove db-specific language

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,7 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-// Create neon connection
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
