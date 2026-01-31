bbdd23ae9b75b8e707bd71211cd53f407501b730
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-01-31 00:32:13 -0500

    feat: add RSVP database schema

---
+++ server/db/migrations/0003_real_maginty.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "households" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"mailing_address" text NOT NULL,
+	"invite_sent" boolean DEFAULT false NOT NULL,
+	"invite_code" varchar(6) DEFAULT lpad(floor(random() * 1000000)::text, 6, '0') NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "households_invite_code_unique" UNIQUE("invite_code")
+);
+--> statement-breakpoint
+CREATE TABLE "members" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"household_id" uuid NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"expected_name" varchar(255),
+	"relationship_type" varchar(20) NOT NULL,
+	"is_attending" boolean,
+	"is_searchable" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "rsvp_responses" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"household_id" uuid NOT NULL,
+	"accommodations" text,
+	"song_recommendation" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "rsvp_responses_household_id_unique" UNIQUE("household_id")
+);
+--> statement-breakpoint
+ALTER TABLE "stories" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
+ALTER TABLE "members" ADD CONSTRAINT "members_household_id_households_id_fk" FOREIGN KEY ("household_id") REFERENCES "public"."households"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "rsvp_responses" ADD CONSTRAINT "rsvp_responses_household_id_households_id_fk" FOREIGN KEY ("household_id") REFERENCES "public"."households"("id") ON DELETE no action ON UPDATE no action;
