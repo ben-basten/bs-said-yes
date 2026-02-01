da245db3b223345bfa5f44209c40d9579cd47453
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-01 00:25:00 -0500

    wip: search UI

--- app/components/form/rsvp/ProgressBar.vue
+++ app/components/form/rsvp/ProgressBar.vue
@@ -3,6 +3,9 @@
     class="absolute top-0 left-0 bg-accent h-2 w-full origin-left transition-transform duration-default ease-default"
     :style="`transform: scaleX(${currentStep / totalSteps})`"
   />
+  <Heading as="p" variant="h4" class="absolute left-1/2 -translate-x-1/2 top-3">
+    Step {{ currentStep }} of {{ totalSteps }}
+  </Heading>
 </template>
 
 <script setup lang="ts">
---
+++ app/components/form/rsvp/StepLookup.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-y-7">
+    <Heading as="h2">Look up your RSVP</Heading>
+    <p class="max-w-copy">
+      Please enter the first and last name of one member of your party below. If
+      you're responding for you and a guest (or your family), you'll be able to
+      RSVP for your entire group on the next step.
+    </p>
+    <FormInput label="First and Last Name" name="name" required />
+    <button type="button" class="button button-lg ml-auto">Search</button>
+  </div>
+</template>
