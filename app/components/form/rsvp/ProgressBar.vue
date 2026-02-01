e47d6936cbbcd8403b58744e71f8037e493b44a9
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-01 00:07:10 -0500

    wip: add progress bar

---
+++ app/components/form/rsvp/ProgressBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="absolute top-0 left-0 bg-accent h-2 w-full origin-left transition-transform duration-default ease-default"
+    :style="`transform: scaleX(${currentStep / totalSteps})`"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  currentStep: number;
+  totalSteps: number;
+}>();
+</script>
