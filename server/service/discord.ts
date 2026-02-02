a04847a7b62ea28b71a8a8e738f34a1a477d92f0
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-02 12:42:07 -0500

    refactor: predictable file locations, eslint console rule, cache navigation

--- server/service/discord.ts
+++ server/service/discord.ts
@@ -1,6 +1,7 @@
 export const postToDiscord = async (message: string) => {
   const url = useRuntimeConfig().discordWebhookUrl;
   if (!url) {
+    // eslint-disable-next-line no-console
     console.warn("Discord webhook URL is not configured.");
     return;
   }
@@ -12,6 +13,7 @@ export const postToDiscord = async (message: string) => {
       },
     });
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error("Error posting to Discord:", error);
   }
 };
