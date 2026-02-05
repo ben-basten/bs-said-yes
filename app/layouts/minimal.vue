7d36ae5f87f8050152fc4d79a55101c9d714b795
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-05 07:25:23 -0500

    wip: add script to login page

--- app/layouts/minimal.vue
+++ app/layouts/minimal.vue
@@ -7,5 +7,15 @@
 <script setup lang="ts">
 useHead({
   meta: [{ name: "robots", content: "noindex, nofollow" }],
+  script: [
+    {
+      innerHTML: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()`,
+    },
+    {
+      src: "https://plausible.io/js/pa-OHkrvpBKZCHL8K-dCydNm.js",
+      async: true,
+    },
+  ],
 });
 </script>
