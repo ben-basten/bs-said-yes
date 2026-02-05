590b1955173bcb3a1def17f57c21732ea7f8b06d
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-05 07:33:09 -0500

    wip: add tracking to default template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -29,5 +29,15 @@ const { data: navigation } = await useFetch<Navigation>("/api/cms/navigation");
 
 useHead({
   meta: [{ name: "robots", content: "noindex, nofollow" }],
+  script: [
+    {
+      innerHTML: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()`,
+    },
+    {
+      src: "https://plausible.io/js/pa-OHkrvpBKZCHL8K-dCydNm.js",
+      async: true,
+    },
+  ],
 });
 </script>
