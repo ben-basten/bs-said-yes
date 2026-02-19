f27c32f0cff28bdf98aeb700032cc619d49a76f2
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-19 18:46:49 -0500

    wip: include date in the guest list

--- server/repository/guests.ts
+++ server/repository/guests.ts
@@ -103,6 +103,7 @@ export const paginatedGuestList = async (
         isAttending: guests.isAttending,
         relationshipType: guests.relationshipType,
         householdNickname: households.nickname,
+        updatedAt: guests.updatedAt,
       })
       .from(guests)
       .leftJoin(households, eq(guests.householdId, households.id))
