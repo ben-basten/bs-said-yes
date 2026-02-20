06401bdb81efef0ea06ea0e67335ecb10563b9fb
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-02-20 09:51:50 -0500

    revert: removing default props from ContentfulImage

--- app/components/base/ContentfulImage.vue
+++ app/components/base/ContentfulImage.vue
@@ -1,10 +1,27 @@
 <template>
-  <NuxtImg provider="contentful" v-bind="props" />
+  <NuxtImg
+    provider="contentful"
+    :src
+    :alt
+    :sizes
+    :quality
+    :fit
+    :format
+    :loading
+  />
 </template>
 
 <script setup lang="ts">
 import { NuxtImg } from "#components";
-const props = defineProps<{
+const {
+  src,
+  alt,
+  sizes,
+  quality = 80,
+  fit = "fill",
+  format = "avif",
+  loading = "lazy",
+} = defineProps<{
   src: string;
   alt: string | undefined;
   sizes: string | undefined;
