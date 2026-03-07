529dc98c598f688aaa6228dc2be521249a434ae8
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-03-07 17:02:23 -0500

    wip: add lite caching to the endpoint

--- server/api/cms/seo.get.ts
+++ server/api/cms/seo.get.ts
@@ -2,7 +2,10 @@ import { fetchContentful } from "~~/server/service/contentful";
 import type { SeoQuery, SeoQueryVariables } from "~~/shared/types/graphql";
 import { SEO_QUERY } from "~~/server/graphql/queries/seo.query";
 
-export default defineEventHandler(async () => {
-  const data = await fetchContentful<SeoQuery, SeoQueryVariables>(SEO_QUERY);
-  return data.componentSeo;
-});
+export default defineCachedEventHandler(
+  async () => {
+    const data = await fetchContentful<SeoQuery, SeoQueryVariables>(SEO_QUERY);
+    return data.componentSeo;
+  },
+  { maxAge: 60, swr: false },
+);
