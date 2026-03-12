8fe00cfeb28a8a1fa88b5cf0cd1137862cdcaab8
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-03-11 20:23:38 -0600

    wip: revert unnecessary changes

--- app/components/form/FormCheckbox.vue
+++ app/components/form/FormCheckbox.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="flex items-center gap-3">
     <input
-      :id="inputId"
+      :id
       v-model="model"
       type="checkbox"
       :name="name"
       :value="value"
       class="size-5 rounded border-2 border-foreground accent-accent cursor-pointer"
     />
-    <label
-      :for="inputId"
-      class="text-lg font-semibold cursor-pointer select-none"
-    >
+    <label :for="id" class="text-lg font-semibold cursor-pointer select-none">
       {{ label }}
     </label>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-  label: string;
-  value?: string;
-  name?: string;
-}>();
-
 const model = defineModel<boolean | string[]>();
+const id = useId();
 
-const fallbackId = useId();
-const inputId = computed(() =>
-  props.value !== undefined ? props.value : fallbackId,
-);
+const { label, name, value } = defineProps<{
+  label: string;
+  name?: string;
+  value?: string;
+}>();
 </script>
